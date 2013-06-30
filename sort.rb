a47b81a92219abc6eb2138ea2ffa0f8153a493b3
Jade McGough <jade@thezets.com>
2013-06-30 01:38:30 -0700

    bubble sort

--- sort.rb
+++ sort.rb
@@ -2,7 +2,7 @@ require 'benchmark'
 
 num_array = []
 
-11.times do
+10000.times do
   num_array << rand(100)
 end
 
@@ -14,8 +14,28 @@ def bogosort(num_array)
   end
 end
 
+# O(n^2) worst case, though O(n) for already sorted arrays
+def bubblesort(num_array)
+  loop do
+    continue_sort = false
+    i = 0
+    while i + 1 < num_array.size
+      if num_array[i] > num_array[i + 1]
+        continue_sort = true
+        num_array[i], num_array[i + 1] = num_array[i + 1], num_array[i]
+      end
+      i += 1
+    end
+    break if !continue_sort
+  end
+end
+
 Benchmark.bm do |bm|
-  bm.report('bogosort') do
-    bogosort(num_array)
+  #not even worth benchmarking - a fairly small n won't (on average) complete before the sun consumes its available hydrogen
+  #bm.report('bogosort') do
+  #  bogosort(num_array)
+  #end
+  bm.report('bubblesort') do
+    bubblesort(num_array.dup)
   end
 end
